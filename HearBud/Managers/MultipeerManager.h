235f9a6aa5dc0bb164b866320e1e7e9c93dca46c
Ritchie Bui <ritchie.bui@gmail.com>
2014-10-23 03:07:32 -0400

    Connection browser and advertiser implemented

---
+++ HearBud/Managers/MultipeerManager.h
@@ -0,0 +1,13 @@
+//
+//  MultipeerManager.h
+//  HearBud
+//
+//  Created by Ritchie Bui on 2014-10-21.
+//  Copyright (c) 2014 Ritchie Bui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MultipeerManager : NSObject
+
+@end
---
+++ HearBud/Managers/MultipeerManager.m
@@ -0,0 +1,13 @@
+//
+//  MultipeerManager.m
+//  HearBud
+//
+//  Created by Ritchie Bui on 2014-10-21.
+//  Copyright (c) 2014 Ritchie Bui. All rights reserved.
+//
+
+#import "MultipeerManager.h"
+
+@implementation MultipeerManager
+
+@end
